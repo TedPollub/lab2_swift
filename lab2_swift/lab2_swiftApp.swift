99f1af1ed884f1e8a67643ea1d4907992c654d26
TedPollub <patryk.zabczynski@pollub.edu.pl>
2024-04-27 14:21:14 +0200

    Initial Commit

---
+++ lab2_swift/lab2_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab2_swiftApp.swift
+//  lab2_swift
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct lab2_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
